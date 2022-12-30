fc484551f6b48b83701877b2e6e55a25c0dbd33a
NataliiaPetrenko <natalia.petrenko15@gmail.com>
2022-12-30 20:49:47 +0200

    Fixed an issue with '0'

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
 let firstNumber = Number(prompt("Hi! Kindly enter the first number:"));
 
-while (!(Number(firstNumber))) {
+function isNumber (number) {
+    return typeof number === 'number' && !isNaN(number);
+}
+
+while (!(isNumber(firstNumber))) {
     firstNumber = Number(prompt("Hi! Kindly enter the first number:"));
 }
 
 let secondNumber = Number(prompt("Hi! Kindly enter the second number:"));
 
-while (!(Number(secondNumber))) {
+while (!(isNumber(secondNumber))) {
     secondNumber = Number(prompt("Hi! Kindly enter the second number:"));
 }
 
 let thirdNumber = Number(prompt("Hi! Kindly enter the third number:"));
 
-while (!(Number(thirdNumber))) {
+while (!(isNumber(thirdNumber))) {
     thirdNumber = Number(prompt("Hi! Kindly enter the third number:"));
 }
 
 const averageNumber = (firstNumber + secondNumber + thirdNumber) / 3
 
 alert (`The average number from ${firstNumber}, ${secondNumber}, ${thirdNumber} is ${averageNumber}`);
+
+
